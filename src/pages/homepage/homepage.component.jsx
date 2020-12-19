5330c220a2d73a4098c2fb5675b4d785640cd136
Aaron Perry <aarongperry@gmail.com>
2020-12-19 15:25:51 -0500

    seperated homepage into smaller components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+	<dvi className='homepage'>
+		<Directory />
+	</dvi>
+);
+
+export default HomePage;
